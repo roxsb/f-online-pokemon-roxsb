b036ccf4b87e4e9d4d9262ed9ebe8d99239c5293
roxsb <rox.san.bri@gmail.com>
2019-03-11 11:32:56 +0100

    change faster url api

--- src/services/PokemonService.js
+++ src/services/PokemonService.js
@@ -1,4 +1,4 @@
-const ENDPOINT = "http://pokeapi.salestock.net/api/v2/pokemon/?offset=0&limit=25";
+const ENDPOINT = "http://pokeapi.co/api/v2/pokemon/?offset=0&limit=25";
 
 const fetchPokemon = () => fetch(ENDPOINT).then(response => response.json());
 
